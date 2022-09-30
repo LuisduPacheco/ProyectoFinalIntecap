2f54280246a6f76e595f0a11219afe0d5962adfe
Geraxxx5 <gerax.pineda@gmail.com>
2022-09-29 21:10:55 -0600

    29/09/2022-21:10-GerardoPineda-Se Agrego un cardview para comprar productos

--- MyAppIntune/app/src/main/java/com/example/myappintune/MaVistaComprarProductos.java
+++ MyAppIntune/app/src/main/java/com/example/myappintune/MaVistaComprarProductos.java
@@ -1,14 +1,17 @@
 package com.example.myappintune;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MaVistaComprarProductos extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ma_vista_comprar_productos);
+        
     }
 }

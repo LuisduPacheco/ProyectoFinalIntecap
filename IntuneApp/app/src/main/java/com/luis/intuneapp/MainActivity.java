1284b7622a01c0ecd2fa180caa3546c67cf48044
LuisduPacheco <luisdu.pacheco@gmail.com>
2022-10-04 17:48:47 -0600

    prueba Luis

--- IntuneApp/app/src/main/java/com/luis/intuneapp/MainActivity.java
+++ IntuneApp/app/src/main/java/com/luis/intuneapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hola Luis, esta es prueba");
+        System.out.println("Hola Zoila esta es una prueba");
     }
 }

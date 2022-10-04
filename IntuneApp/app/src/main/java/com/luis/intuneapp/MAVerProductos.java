2a847291a96d4df9ee8915e8c7afa733589f3301
LuisduPacheco <luisdu.pacheco@gmail.com>
2022-10-03 21:01:25 -0600

    Nuevo proyecto android

---
+++ IntuneApp/app/src/main/java/com/luis/intuneapp/MAVerProductos.java
@@ -0,0 +1,14 @@
+package com.luis.intuneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MAVerProductos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maver_productos);
+    }
+}
